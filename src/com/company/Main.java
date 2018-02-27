563dece99ac8b7e03a2df59a40d12c8bcc625334
siham-sharif <siham.sharif@gmail.com>
2018-02-27 21:37:55 +0600

    Basic Swing done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Hello World");
+
+                frame.setSize(600, 500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
     }
 }
