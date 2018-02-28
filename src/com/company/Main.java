859317fe518adddbb235ac5b1bb2129d3fa1aec8
siham-sharif <siham.sharif@gmail.com>
2018-02-28 12:19:42 +0600

    Border Layout work done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,9 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new JFrame("Hello World");
 
-                frame.setSize(600, 500);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
+                new MainFrame();
+
             }
         });
     }
---
+++ src/com/company/MainFrame.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    private JTextArea textArea;
+    private JButton btn;
+
+    public MainFrame(){
+
+        super("Hello World");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Click me!");
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+}
