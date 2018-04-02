a3a163f4ff42bc5f73cf9ccac0d69170e10a4477
siham-sharif <siham.sharif@gmail.com>
2018-04-02 17:45:51 +0600

    Added Jlabel JtextField to Form

--- src/com/company/FormPanel.java
+++ src/com/company/FormPanel.java
@@ -5,10 +5,26 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class FormPanel extends JPanel {
+
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okBtn;
+
+
     public FormPanel(){
         Dimension dim = getPreferredSize();
         dim.width=250;
         setPreferredSize(dim);
+
+        nameLabel = new JLabel("Name: ");
+        occupationLabel = new JLabel("Occupation: ");
+        nameField = new JTextField(10); // how many characters
+        occupationField = new JTextField(10);
+
+        okBtn = new JButton("OK");
+
 
         //Adding Border
         Border innerBorder = BorderFactory.createTitledBorder("Add Person");
